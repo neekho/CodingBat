f993af1da3a1247164b36f96e616f8ec7dc98cb6
Nico <remullanico28@gmail.com>
2024-01-28 23:35:43 +0800

    solved 2-3 problems

--- Map1/Main.java
+++ Map1/Main.java
@@ -13,6 +13,7 @@ public class Main {
     }};
 
 
+    // 1
     public static Map<String, String>  mapBully(Map<String, String> map) {
 
 
@@ -31,6 +32,7 @@ public class Main {
     }
     
 
+    // 2
     public static Map<String, String> topping1(Map<String, String> map) {
 
         if (map.containsKey("bread") || !map.containsKey("bread")) {
@@ -46,6 +48,69 @@ public class Main {
         return map;
   
     }
+
+
+    // 3
+    public static Map<String, String> mapAB2(Map<String, String> map) {
+        
+        boolean bothPresent = map.containsKey("a") && map.containsKey("b");
+        
+        if (bothPresent) {
+            boolean sameValue = map.get("a").equals(map.get("b"));
+
+            if (sameValue) {
+                map.remove("a");
+                map.remove("b");
+
+                return map;
+            }
+        }
+
+
+
+        return map;
+    }
+    
+
+    // 4
+    
+
+    // 5 
+    public static Map<String, String> topping2(Map<String, String> map) {
+
+        boolean iceCream = map.containsKey("ice cream") && map.get("ice cream") != null;
+        boolean spinach = map.containsKey("spinach") && map.get("spinach") != null;
+
+
+        if (iceCream) 
+            map.put("yogurt", map.get("ice cream"));
+
+        if (spinach)
+            map.put("spinach", "nuts");
+
+
+
+
+        return map;
+    }
+
+
+    // 6
+    public static Map<String, String> mapAB3(Map<String, String> map) {
+
+        boolean aValue = map.containsKey("a") && map.get("a") != null;
+        boolean bValue = map.containsKey("b") && map.get("b") != null;
+
+        if (aValue && !bValue)
+            map.put("b", map.get("a"));
+            
+        if (bValue && !aValue)
+            map.put("a", map.get("b"));
+
+        return map;
+  
+    }
+    
     
 
 
