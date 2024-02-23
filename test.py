c3f5fe05a218d1223b69050e4f5c8c604cbda818
Nico <remullanico28@gmail.com>
2024-02-23 23:49:17 +0800

    partially solved hackerrank question during ibm int

--- test.py
+++ test.py
@@ -1,27 +1,7 @@
-
-valid_tokens = ['zxcxvvb12', 'afsgerr2c']
-
-
-requests = [['GET', 'https://www.example.com/?token=afsgerr2c&name=alex'], 
-            ['GET', 'https://www.wikipedia.com/?token=zxfcawsdgear&name=alex'], 
-            ['POST', 'https://www.facebook.com/?token=sfhsdvc23&name=nikho&city=makati&age=24']
-            
-            
-            ]
-
-def get_request_token(url):
-
-    start = url.find('token=')
-
-    end = url.find('&') if '&' in url else len(url)
-    
-    token = url[start:end].split('token=')[1]
-
-    return token
+import re
+from urllib.parse import urlparse, parse_qs
 
 
-def valid_csrf(url):
-    pass
 
 
 
@@ -33,26 +13,69 @@ def getRequest(valid_auth_tokens, requests) -> list:
 
     for request in requests:
 
+        
+
         method = request[0]
         url = request[1]
         domain = url.split('?token=')[0]
+        
         request_token = get_request_token(url)
 
         valid_token = request_token in valid_auth_tokens
 
 
-        if method == 'GET' and valid_token:
+        if method == 'GET':
+
+            if valid_token: 
+
+
+                parsed_url = urlparse(url)
+                query_params = parse_qs(parsed_url.query)
+                
+
+                
+                response.append(f'VALID {", ".join(f"{key}, {value}" for key, value in query_params.items())}')
+
+            else: response.append('INVALID')
+
+        elif method == 'POST' and valid_token and valid_csrf(request_token):
             response.append('VALID')
 
-    
+        else: response.append('INVALID')
 
 
 
 
     return response
 
+def get_request_token(url) -> str:
+
+    start = url.find('token=')
+
+    end = url.find('&') if '&' in url else len(url)
+    
+    token = url[start:end].split('token=')[1]
+
+    return token
 
 
+def valid_csrf(token) -> bool:
+    pattern = "^[a-z0-9]+$"
+    return bool(re.match(pattern, token))
+
+
+
+# RUN
+
+valid_tokens = ['zxcxvvb12', 'afsgerr2c']
+
+
+requests = [['GET', 'https://www.example.com/?token=afsgerr2c&name=alex&age=22&city=quezon'], 
+            ['GET', 'https://www.wikipedia.com/?token=zxfcawsdgear&name=alex'], 
+            ['POST', 'https://www.facebook.com/?token=sfhsdvc23&name=nikho&city=makati&age=24'],
+            ['POST', 'https://www.facebook.com/?token=zxcxvvb12&name=nikho&city=makati&age=24']
+            
+            ]
 
 
 r = getRequest(valid_tokens, requests)
